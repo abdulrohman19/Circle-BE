6d72c9f249ea31e0b977710d1f234d1dfa73caf2
abdul rohman <abdulrohmanunida@gmail.com>
2024-10-24 20:36:50 +0700

    Update user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -17,19 +17,77 @@ export const getUser = async (req: Request, res: Response) => {
 export const createUser = async (req: Request, res: Response) => {
   try {
     // const body = req.body
-  const { body } = req;
-  console.log(body);
+    const { body } = req;
+    console.log(body);
 
-  const addedUser = await userServices.createUser(body);
+    const addedUser = await userServices.createUser(body);
 
-  res.json(addedUser);
-    
+    res.json(addedUser);
   } catch (error) {
     console.log(error);
 
     res.status(500).json({
       message: (error as Error).message,
-    });    
+    });
   }
-  
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const { id } = req.params;
+    const updateUser = await userServices.updateUser({ ...body, id: +id });
+
+    res.json(updateUser);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: (error as Error).message,
+    });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await userServices.deleteUser(parseInt(id));
+
+    res.json(deleteUser);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: (error as Error).message,
+    });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await userServices.findUniqueUser(parseInt(id));
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: (error as Error).message,
+    });
+  }
+};
+
+export const getUserByQuery = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.query;
+    const user = await userServices.findUserByName(name as string);
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: (error as Error).message,
+    });
+  }
 };
