2192f549a538c2da1ff11f86634269eceb55d7bf
abdul rohman <abdulrohmanunida@gmail.com>
2024-10-25 06:21:10 +0700

    Update userRouter

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 import * as userController from "../controllers/user";
 const userRouter = Router();
 
-
 userRouter.get("/", userController.getUser);
 userRouter.post("/", userController.createUser);
+userRouter.patch("/:id", userController.updateUser);
+userRouter.delete("/:id", userController.deleteUser);
 
-export default userRouter
+userRouter.get("/detail/:id", userController.getUserById);
+userRouter.get("/search", userController.getUserByQuery);
+
+export default userRouter;
